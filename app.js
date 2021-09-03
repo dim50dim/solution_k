c6642153b742e412909fc195a328c1ad522f4c53
dim50dim <zheliezkov@gmail.com>
2021-09-03 08:38:55 +0300

    one change

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const path = require('path');
+
+
 
 const PORT =  5000;
 
 const app = express();
+const staticPath = path.join(__dirname,'static');
 
-app.use(express.static('static'));
 
-app.get('/',(res,req) => {
-    res.render('index.html') ;
-});
+app.use(express.static(staticPath));
+
+
+const { pingRouter,
+        userRouter} = require('./routers');
+
+app.use('/ping',pingRouter);
+app.use('/user',userRouter);
 
 app.listen(PORT, (res,req) => {
     console.log('WE ARE LISTENIG PORT',PORT);
